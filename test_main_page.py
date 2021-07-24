651145c721d9402fa786b99af8d181fe88b1826e
RushanUsmanov <Rushan.Usmaanov@yandex.ru>
2021-07-25 00:10:32 +0300

    Add files from lesson 4.2 step 8 - correct case for the login and registration page

--- test_main_page.py
+++ test_main_page.py
@@ -1,9 +1,12 @@
 from .pages.main_page import MainPage
+from .pages.login_page import *
 
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
+    login = LoginPage(browser, link)
     page.open()
     page.go_to_login_page()
     page.should_be_login_link()
+    login.should_be_login_page()
